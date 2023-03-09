23c15afb7b35f5b4f08ca4b12954f903f1647e33
jberque <jberque@snapp.fr>
2023-03-09 15:51:00 +0100

    Updating rule EREA001 SupportedVersionRange adding short targetSdk / minSdk coverage

--- android-plugin/src/test/java/org/sonar/plugins/groovy/GroovySonarWayProfileTest.java
+++ android-plugin/src/test/java/org/sonar/plugins/groovy/GroovySonarWayProfileTest.java
@@ -18,11 +18,6 @@
  */
 package org.sonar.plugins.groovy;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.junit.Test;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition.BuiltInActiveRule;
@@ -30,45 +25,68 @@ import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.utils.ValidationMessages;
 import org.sonar.plugins.groovy.codenarc.CodeNarcRulesDefinition;
 import org.sonar.plugins.groovy.foundation.Groovy;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GroovySonarWayProfileTest {
 
-  @Test
-  public void shouldCreateSonarWayProfile() {
-    ValidationMessages messages = ValidationMessages.create();
+    @Test
+    public void shouldCreateSonarWayProfile() {
+        ValidationMessages messages = ValidationMessages.create();
 
-    GroovySonarWayProfile profileDef = new GroovySonarWayProfile();
-    BuiltInQualityProfilesDefinition.Context profileContext =
-        new BuiltInQualityProfilesDefinition.Context();
-    profileDef.define(profileContext);
-    BuiltInQualityProfilesDefinition.BuiltInQualityProfile profile =
-        profileContext.profile(Groovy.KEY, Groovy.PROFILE_NAME);
-    assertThat(profile.language()).isEqualTo(Groovy.KEY);
-    List<BuiltInActiveRule> activeRules = profile.rules();
-    // TODO The Number of Custom profile rules are set here,
-    //  we need to change this to get the size dynamically
-    assertThat(activeRules).as("Expected number of rules in profile").hasSize(3);
-    assertThat(profile.name()).isEqualTo(Groovy.PROFILE_NAME);
+        GroovySonarWayProfile profileDef = new GroovySonarWayProfile();
+        BuiltInQualityProfilesDefinition.Context profileContext = new BuiltInQualityProfilesDefinition.Context();
+        profileDef.define(profileContext);
+        BuiltInQualityProfilesDefinition.BuiltInQualityProfile profile = profileContext.profile(Groovy.KEY, Groovy.PROFILE_NAME);
+        assertThat(profile.language()).isEqualTo(Groovy.KEY);
+        List<BuiltInActiveRule> activeRules = profile.rules();
 
-    // Check that we use severity from the read rule and not default one.
-    assertThat(activeRules.get(0).overriddenSeverity()).isNull();
-    assertThat(messages.hasErrors()).isFalse();
+        // Here we check the rules.xml file to get the number of rules in it
+        File profile_default = new File("src/main/resources/org/sonar/plugins/groovy/rules.xml");
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db;
+        try {
+            db = dbf.newDocumentBuilder();
+            Document doc = db.parse(profile_default);
+            doc.getDocumentElement().normalize();
+            NodeList rule = doc.getElementsByTagName("rule");
+            assertThat(activeRules).as("Expected number of rules in profile").hasSize(rule.getLength());
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(profile.name()).isEqualTo(Groovy.PROFILE_NAME);
 
-    // Check that all rules in "Sonar way" actually exist
-    CodeNarcRulesDefinition definition = new CodeNarcRulesDefinition();
-    RulesDefinition.Context rulesContext = new RulesDefinition.Context();
-    definition.define(rulesContext);
-    RulesDefinition.Repository repository =
-        rulesContext.repository(CodeNarcRulesDefinition.REPOSITORY_KEY);
+        // Check that we use severity from the read rule and not default one.
+        assertThat(activeRules.get(0).overriddenSeverity()).isNull();
+        assertThat(messages.hasErrors()).isFalse();
 
-    Set<String> rules = new HashSet<>();
-    for (RulesDefinition.Rule rule : repository.rules()) {
-      rules.add(rule.key());
+        // Check that all rules in "Sonar way" actually exist
+        CodeNarcRulesDefinition definition = new CodeNarcRulesDefinition();
+        RulesDefinition.Context rulesContext = new RulesDefinition.Context();
+        definition.define(rulesContext);
+        RulesDefinition.Repository repository =
+                rulesContext.repository(CodeNarcRulesDefinition.REPOSITORY_KEY);
+
+        Set<String> rules = new HashSet<>();
+        for (RulesDefinition.Rule rule : repository.rules()) {
+            rules.add(rule.key());
+        }
+        for (BuiltInActiveRule activeRule : profile.rules()) {
+            assertThat(rules.contains(activeRule.ruleKey()))
+                    .as("No such rule: " + activeRule.ruleKey())
+                    .isTrue();
+        }
     }
-    for (BuiltInActiveRule activeRule : profile.rules()) {
-      assertThat(rules.contains(activeRule.ruleKey()))
-          .as("No such rule: " + activeRule.ruleKey())
-          .isTrue();
-    }
-  }
 }
